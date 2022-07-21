7ec20315a883f5ea83d17f272b24d6c221158f91
Mohamad Alsyouf <mohamadalsyouf99@gmail.com>
2022-07-21 02:04:22 -0700

    Completed Ezoic Final Assessment!

--- client/src/Chat.js
+++ client/src/Chat.js
@@ -1,29 +1,32 @@
-import React from 'react';
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
+import ScrollToBottom from 'react-scroll-to-bottom';
 
 function Chat({socket, userName, room}) {
   const [currentMessage, setCurrentMessage] = useState("")
   const [messageList, setMessageList] = useState([]);
 
   const sendMessage = async () => {
+    const date = new Date();
+    console.log(date);
     if (currentMessage !== "") {
       const messageData = {
         room: room,
         author: userName,
         message: currentMessage,
-        time:
-          new Date(Date.now()).getHours() +
-          ":" +
-          new Date(Date.now()).getMinutes()
+        time: new Intl.DateTimeFormat('en-US', {timeStyle: "short"}).format(date)
       }
+      console.log(messageData.time);
+      console.log(messageData);
       await socket.emit("send_message", messageData)
-      // also sets messageList not only when we receive a message, but when
-      // we send a message as well.
+      // line 23 sets messageList not only when we receive
+      // a message(31), but when we send a message as well.
       setMessageList((list) => [...list, messageData])
+      setCurrentMessage("");
     }
   };
 
-  // when message event is emitted, keeps track of previous data (messages)
+  // this message event is emitted(server),
+  // keeps track of previous data (messages)
   useEffect(() => {
     socket.on("receive_message", (data) => {
       setMessageList((list) => [...list, data])
@@ -33,32 +36,36 @@ function Chat({socket, userName, room}) {
   return (
     <div className="chat-window">
       <div className="chat-header">
-        <p>Live Chat</p>
+        <p>Ezoic Chat</p>
       </div>
       <div className="chat-body">
-        {messageList.map((messageContent) => {
-          return (
-          <div
-            className="message"
-            id={userName === messageContent.author ? "other" : "you"}
-          >
-            <div>
-              <div className="message-content">
-                <p>{messageContent.message}</p>
-              </div>
-              <div className="message-meta">
-                <p id="time">{messageContent.time}</p>
-                <p id="author">{messageContent.author}</p>
+        <ScrollToBottom className="message-container">
+          {messageList.map((messageContent, index) => {
+            return (
+            <div
+              className="message"
+              id={userName === messageContent.author ? "other" : "you"}
+              key={index}
+            >
+              <div>
+                <div className="message-content">
+                  <p>{messageContent.message}</p>
+                </div>
+                <div className="message-meta">
+                  <p id="time">{messageContent.time}</p>
+                  <p id="author">{messageContent.author}</p>
+                </div>
               </div>
             </div>
-          </div>
-          )
-        })}
+            )
+          })}
+        </ScrollToBottom>
       </div>
       <div className="chat-footer">
         <input
           type="text"
-          placeholder="Hey..."
+          value={currentMessage}
+          placeholder="Chat..."
           onChange={(event) => {
             setCurrentMessage(event.target.value);
           }}
--- server/index.js
+++ server/index.js
@@ -21,20 +21,19 @@ io.on("connection", (socket) => {
   socket.on("join_room", (data) => {
     socket.join(data);
     console.log(`User with ID: ${socket.id} joined room: ${data}`);
+    console.log("data: ", data);
   });
-
   // whenever someone types a message (in Chat.js), they emit the send message event and
   // send the data here, then this will emit the message sent to everyone listening for message
   socket.on("send_message", (data) => {
     socket.to(data.room).emit("receive_message", data);
+    console.log("AUTHOR: ", data.author);
   });
 
   socket.on("disconnect", () => {
     console.log("User Disconnected", socket.id)
   });
-
 });
-
 
 server.listen(3001, () => {
   console.log("SERVER RUNNING")
